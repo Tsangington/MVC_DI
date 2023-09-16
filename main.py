38f346f2cc41a8c1405bfde69859eba2d84be82c
Tsangington <harry.tsang@code.berlin>
2023-09-16 16:59:22 +0200

    Attempt at using DI framework

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from view import View
 from controller import Controller
 from config import Config
 import logging
+from dependency_injector import containers
+from .containers import Container
 
 logging.basicConfig(filename='logs.txt', encoding='utf-8', level=logging.DEBUG)
 
 #Own Dependency injection?
-
-if __name__ == "__main__":
+def main():
     config = Config()
     model = Model(config)
     view = View()
     controller = Controller( model, view, config)
+
+if __name__ == "__main__":
+    #container = Container()
+
+    main()
